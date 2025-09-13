d5120597659f61734c2a76144e36c1904ec6d545
Denzil Serrao <115720007+DenzilSerrao@users.noreply.github.com>
2025-09-13 09:26:32 +0530

    NativeWind is now in working

--- babel.config.js
+++ babel.config.js
@@ -2,16 +2,9 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: [
-      [
-        'babel-preset-expo',
-        {
-          jsxImportSource: 'nativewind',
-        },
-      ],
-    ],
-    plugins: [
+      ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
       'nativewind/babel',
-      'react-native-reanimated/plugin',
     ],
+    plugins: ['react-native-reanimated/plugin'],
   };
 };
